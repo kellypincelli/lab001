3d2e4fd20d5fb4f2b2b58fecb518dc6375c750a4
Angular CLI <angular-cli@angular.io>
2017-08-14 17:31:12 -0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { LabPage } from './app.po';
+
+describe('lab App', () => {
+  let page: LabPage;
+
+  beforeEach(() => {
+    page = new LabPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
